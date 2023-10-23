34bdfd9818ef32cd0aa768c2640becbc152063ad
Alla-ud-din <allauddin.aliahmad9@gmail.com>
2023-10-23 21:12:07 +0500

    adding initial files

---
+++ Python Crash Course/class-01/class1.py
@@ -0,0 +1,7 @@
+name : tuple[str,int,float] = ('pakistan',7,2.0)
+print(name) # print
+print(type(name)) # type
+print(id(name)) # physcial address
+print([i for i in dir(name) if "__" not in i]) # methods and attributes
+
+
